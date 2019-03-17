0823def3f53dc3daa1385793e5cf7a1fb112b878
Sean Lin <seanlin1016@gmail.com>
2019-03-17 15:50:53 +0800

    Initial Project Setup

---
+++ SwiftAutolayoutLib/SwiftAutolayoutLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAutolayoutLib.h
+//  SwiftAutolayoutLib
+//
+//  Created by SEAN on 2019/3/17.
+//  Copyright © 2019 seanlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftAutolayoutLib.
+FOUNDATION_EXPORT double SwiftAutolayoutLibVersionNumber;
+
+//! Project version string for SwiftAutolayoutLib.
+FOUNDATION_EXPORT const unsigned char SwiftAutolayoutLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAutolayoutLib/PublicHeader.h>
+
+
---
+++ SwiftAutolayoutLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftAutolayoutLibExample
+//
+//  Created by SEAN on 2019/3/17.
+//  Copyright © 2019 seanlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
